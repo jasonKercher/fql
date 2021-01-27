cd3c394c76ea62210f5368489ccd3fbc41780d17
jasonkercher <jkercher43@gmail.com>
2021-01-27 11:18:49 -0500

    logic test fails

--- tests/check_logic.c
+++ tests/check_logic.c
@@ -15,7 +15,7 @@ ff5487af,f4,75882
 f99b9313,22,229701
 */
 
-void assert_select_1(int plan_count)
+void assert_select1(int plan_count)
 {
         struct fql_field* fields = NULL;
         int field_count = 0;
@@ -56,90 +56,103 @@ void assert_nothing(int plan_count)
 START_TEST(test_logic_const)
 {
         // fql_logic_eq_i
-        assert_select_1(fql_make_plans(fql, "select 1 where '01' = 1"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -1 = 1"));
+        assert_select1(fql_make_plans(fql, "select 1 where '01' = 1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -1 = 1"));
 
         // fql_logic_eq_f
-        assert_select_1(fql_make_plans(fql, "select 1 where 50.5 = '050.5'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 50.4 = 50.5"));
+        assert_select1(fql_make_plans(fql, "select 1 where 50.5 = '050.5'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 50.4 = 50.5"));
 
         // fql_logic_eq_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'hi' = 'Hi   '"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 'hi' = ' hi'"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'hi' = 'Hi   '"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 'hi' = ' hi'"));
 
         // fql_logic_ne_i
-        assert_select_1(fql_make_plans(fql, "select 1 where 1 != 0"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 9223372036854775807 <> 9223372036854775807"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1 != 0"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 9223372036854775807 <> 9223372036854775807"));
 
         // fql_logic_ne_f
-        assert_select_1(fql_make_plans(fql, "select 1 where -.0001 != -.00011"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 33.333 <> 33.333"));
+        assert_select1(fql_make_plans(fql, "select 1 where -.0001 != -.00011"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 33.333 <> 33.333"));
 
         // fql_logic_ne_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'abcde f' != 'abcde'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where '' <> ''"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'abcde f' != 'abcde'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where '' <> ''"));
 
         // fql_logic_gt_i
-        assert_select_1(fql_make_plans(fql, "select 1 where 5 > -6"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -9223372036854775808 > 9223372036854775807"));
+        assert_select1(fql_make_plans(fql, "select 1 where 5 > -6"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -9223372036854775808 > 9223372036854775807"));
 
         // fql_logic_gt_f
-        assert_select_1(fql_make_plans(fql, "select 1 where 1.1 > 1"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -1.1 > 1"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1.1 > 1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -1.1 > 1"));
 
         // fql_logic_gt_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'Bad' > 'abc'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 'A' > '{'")); /* THIS BREAKS FROM T-SQL */
+        assert_select1(fql_make_plans(fql, "select 1 where 'Bad' > 'abc'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 'A' > '{'")); /* THIS BREAKS FROM T-SQL */
 
         // fql_logic_ge_i
-        assert_select_1(fql_make_plans(fql, "select 1 where 5 >= 5"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 5 >= '4'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -1 >= 1"));
+        assert_select1(fql_make_plans(fql, "select 1 where 5 >= 5"));
+        assert_select1(fql_make_plans(fql, "select 1 where 5 >= '4'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -1 >= 1"));
 
         // fql_logic_ge_f
-        assert_select_1(fql_make_plans(fql, "select 1 where -22.1 >= -31"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 22.1 >= 22.1"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -22.10001 >= -22.1"));
+        assert_select1(fql_make_plans(fql, "select 1 where -22.1 >= -31"));
+        assert_select1(fql_make_plans(fql, "select 1 where 22.1 >= 22.1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -22.10001 >= -22.1"));
 
         // fql_logic_ge_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'abc' >= '[c]   '"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 'abc' >= 'abc   '"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 'abc' >= 'abc d'"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'abc' >= '[c]   '"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'abc' >= 'abc   '"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 'abc' >= 'abc d'"));
 
         // fql_logic_lt_i
-        assert_select_1(fql_make_plans(fql, "select 1 where -1 < 1"));
-        assert_nothing( fql_make_plans(fql, "select 1 where '1' < -1"));
+        assert_select1(fql_make_plans(fql, "select 1 where -1 < 1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where '1' < -1"));
 
         // fql_logic_lt_f
-        assert_select_1(fql_make_plans(fql, "select 1 where 5.992 < 5.993"));
-        assert_nothing( fql_make_plans(fql, "select 1 where -11 < -12.1"));
+        assert_select1(fql_make_plans(fql, "select 1 where 5.992 < 5.993"));
+        assert_nothing(fql_make_plans(fql, "select 1 where -11 < -12.1"));
 
         // fql_logic_lt_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'shnt' < 'TNHS'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 'shnt' < 'SHNT'"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'shnt' < 'TNHS'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 'shnt' < 'SHNT'"));
 
         // fql_logic_le_i
-        assert_select_1(fql_make_plans(fql, "select 1 where 1 <= 1"));
-        assert_select_1(fql_make_plans(fql, "select 1 where -12341 <= 1"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 0 <= -1"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1 <= 1"));
+        assert_select1(fql_make_plans(fql, "select 1 where -12341 <= 1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 0 <= -1"));
 
         // fql_logic_le_f
-        assert_select_1(fql_make_plans(fql, "select 1 where -888.1234 <= 888"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 888.0 <= 888"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 888.1234 <= 888"));
+        assert_select1(fql_make_plans(fql, "select 1 where -888.1234 <= 888"));
+        assert_select1(fql_make_plans(fql, "select 1 where 888.0 <= 888"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 888.1234 <= 888"));
 
         // fql_logic_le_s
-        assert_select_1(fql_make_plans(fql, "select 1 where 'hello world!' <= 'hello world!'"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 'hello world' <= 'hello world!'"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 'hello_world!' <= 'hello world!'"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'hello world!' <= 'hello world!'"));
+        assert_select1(fql_make_plans(fql, "select 1 where 'hello world' <= 'hello world!'"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 'hello_world!' <= 'hello world!'"));
 }
 END_TEST
 
 START_TEST(test_logic_function)
 {
-        assert_select_1(fql_make_plans(fql, "select 1 where left('hello', 2) = 'he    '"));
-        assert_select_1(fql_make_plans(fql, "select 1 where 1000 + 233 < right('hi1234', 4)"));
-        assert_nothing( fql_make_plans(fql, "select 1 where 100 * -1 > 0 / 1000"));
+        assert_select1(fql_make_plans(fql, "select 1 where left('hello', 2) = 'he    '"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1000 + 233 < right('hi1234', 4)"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 100 * -1 > 0 / 1000"));
+}
+
+START_TEST(test_logic_complex)
+{
+        assert_select1(fql_make_plans(fql, "select 1 where 1=1 and 1=1"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1=1 or 1=0"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 1=1 and 1=0"));
+        assert_nothing(fql_make_plans(fql, "select 1 where 1=0 or 1=1 and 1=0"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1=1 or 1=1 and 1=0"));
+        assert_nothing(fql_make_plans(fql, "select 1 where (1=1 or 1=1) and 1=0"));
+        assert_select1(fql_make_plans(fql, "select 1 where 1=0 and 1=1 or 1=1"));
+        assert_nothing(fql_make_plans(fql, "select 1 where (1=1 and 1=0) or (1=1 and 1=0)"));
+        assert_select1(fql_make_plans(fql, "select 1 where (1=1 and 1=0) or (1=1 and 1=1)"));
 }
 
 Suite* fql_logic_suite(void)
@@ -152,7 +165,7 @@ Suite* fql_logic_suite(void)
 
         tcase_add_test(tc_logic, test_logic_const);
         tcase_add_test(tc_logic, test_logic_function);
-        //tcase_add_test(tc_logic, test_logic_on_read);
+        tcase_add_test(tc_logic, test_logic_complex);
 
         suite_add_tcase(s, tc_logic);
 
