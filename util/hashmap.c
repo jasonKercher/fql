5d694217d8cb1fa0e6cd332a46a7a15d09ff5903
jasonkercher <jkercher43@gmail.com>
2021-04-01 17:11:48 -0400

    more hash map work

--- util/hashmap.c
+++ util/hashmap.c
@@ -1,6 +1,10 @@
 #include "hashmap.h"
 #include "util.h"
 
+/* Hashing based on FNV-1 */
+const uint64_t _OFFSET = 14695981039346656037UL;
+const uint64_t _PRIME = 1099511628211UL;
+
 HashMap* hashmap_new(size_t limit, unsigned props)
 {
         HashMap* new_map = NULL;
@@ -11,14 +15,19 @@ HashMap* hashmap_new(size_t limit, unsigned props)
 HashMap* hashmap_construct(HashMap* m, size_t limit, unsigned props)
 {
         *m = (HashMap) {
-                 NULL   /* data */
-                ,{ 0 }  /* _keybuf */
+                 NULL   /* _entries */
+                ,limit  /* _size */
+                ,NULL   /* _keybuf */
                 ,0      /* _keybufhead */
+                ,limit  /* _keybuflen */
                 ,props  /* props */
         };
 
-        malloc_(m->_data, sizeof(void*) * limit);
-        vec_construct_(&m->_keybuf, char);
+        malloc_(m->_entries, sizeof(struct hm_entry) * limit);
+        memset(m->_entries, 0, sizeof(struct hm_entry) * limit);
+
+        malloc_(m->_keybuf, limit);  /* Expect this to grow */
+
         return m;
 }
 
@@ -30,18 +39,75 @@ void hashmap_free(HashMap* m)
 
 void hashmap_destroy(HashMap* m)
 {
-        free_(m->_data);
-        vec_destroy(&m->_keybuf);
+        free_(m->_entries);
+        free_(m->_keybuf);
 }
 
-void* hashmap_nset(HashMap* m, const char* key, void* data, int n)
+uint64_t _nhash(HashMap* m, const char* key, int n)
 {
-        return NULL;
+        uint64_t hash = _OFFSET;
+        char* keyptr = &m->_keybuf[m->_keybufhead];
+        int i = 0;
+
+        for (; i < n; ++i) {
+                *keyptr = key[i];
+                hash *= _PRIME;
+                hash ^= (uint64_t) *keyptr;
+        }
+
+        return hash;
 }
 
-void* hashmap_set(HashMap* m, const char* key, void* data)
+uint64_t _nhash_nocase(HashMap* m, const char* key, int n)
 {
-        return NULL;
+        uint64_t hash = _OFFSET;
+        char* keyptr = &m->_keybuf[m->_keybufhead];
+        int i = 0;
+
+        for (; i < n; ++i) {
+                *keyptr = tolower(key[i]);
+                hash *= _PRIME;
+                hash ^= (uint64_t) *keyptr;
+        }
+
+        return hash;
+}
+
+void hashmap_nset(HashMap* m, const char* key, void* data, int n)
+{
+        if (m->_keybufhead + n > m->_keybuflen) {
+                m->_keybuflen *= 2;
+                realloc_(m->_keybuf, n + m->_keybuflen);
+        }
+
+        uint64_t hash = 0;
+        if (m->props & HASHMAP_PROP_NOCASE) {
+                hash = _nhash_nocase(m, key, n);
+        } else {
+                hash = _nhash(m, key, n);
+        }
+
+        size_t idx = (size_t)(hash & (m->_limit-1));
+
+        /* use memcmp instead of strcmp in case non-char* key */
+        struct hm_entry* entry = &m->_entries[idx];
+        while (entry->value != NULL && 
+               entry->keylen == n &&
+               memcmp(&m->_keybuf[entry->keyidx], &m->_keybuf[m->_keybufhead], n) == 0) {
+                idx = (idx + 1) % m->_limit;
+                entry = &m->_entries[idx];
+        }
+
+        if (entry->value == NULL) {  /* New value */
+                entry->keyidx = m->_keybufhead;
+                entry->keylen = n;
+                m->_keybufhead += n;
+        } 
+        entry->value = data;
+}
+
+void hashmap_set(HashMap* m, const char* key, void* data)
+{
 }
 
 void* hashmap_nget(HashMap* m, const char* key, int n)
--- util/hashmap.h
+++ util/hashmap.h
@@ -2,6 +2,7 @@
 #define HASHMAP_H
 
 #include <stdlib.h>
+#include <stdint.h>
 #include "vec.h"
 
 #define HASHMAP_PROP_DEFAULT 0x00
@@ -15,9 +16,11 @@ struct hm_entry {
 };
 
 struct hashmap {
-        void* _data;
-        struct vec _keybuf;
+        struct hm_entry* _entries;
+        uint64_t _limit;
+        char* _keybuf;
         size_t _keybufhead;
+        size_t _keybuflen;
         unsigned props;
 };
 typedef struct hashmap HashMap;
@@ -26,8 +29,8 @@ struct hashmap* hashmap_new(size_t limit, unsigned props);
 struct hashmap* hashmap_construct(struct hashmap*, size_t limit, unsigned props);
 void hashmap_free(struct hashmap*);
 void hashmap_destroy(struct hashmap*);
-void* hashmap_nset(struct hashmap*, const char* key, void*, int);
-void* hashmap_set(struct hashmap*, const char* key, void*);
+void hashmap_nset(struct hashmap*, const char* key, void*, int);
+void hashmap_set(struct hashmap*, const char* key, void*);
 void* hashmap_nget(struct hashmap*, const char* key, int);
 void* hashmap_get(struct hashmap*, const char* key);
 
